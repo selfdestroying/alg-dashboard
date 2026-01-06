775d8842f2cca58443c8628ee13f9fdce8589565
selfdestroying <max.f99@yandex.ru>
2026-01-06 20:42:16 +0300

    feat: refactor active students table and update data fetching logic

--- src/app/dashboard/students/active/page.tsx
+++ src/app/dashboard/students/active/page.tsx
@@ -1,6 +1,6 @@
 import { getActiveStudents } from '@/actions/students'
-import { ActiveStudentsTable } from '@/components/tables/active-students-table'
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { ActiveStudentsTable } from './_components/active-students-table'
 
 export default async function Page() {
   const students = await getActiveStudents()
