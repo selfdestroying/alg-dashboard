a54b0fa65d8cd4d09af3d5f5076e334dac3a0d65
selfdestroying <max.f99@yandex.ru>
2026-01-24 20:21:18 +0300

    refactor: enhance category management with CRUD operations and UI components

--- src/actions/categories.ts
+++ src/actions/categories.ts
@@ -4,18 +4,23 @@ import { prisma } from '@/lib/prisma'
 import { Prisma } from '@prisma/client'
 import { revalidatePath } from 'next/cache'
 
-export const getCategories = async () => {
-  return await prisma.category.findMany()
+export const getCategories = async <T extends Prisma.CategoryFindManyArgs>(
+  payload?: Prisma.SelectSubset<T, Prisma.CategoryFindManyArgs>
+) => {
+  return await prisma.category.findMany(payload)
 }
 
-export const createCategory = async (
-  data: Prisma.XOR<Prisma.CategoryCreateInput, Prisma.CategoryUncheckedCreateInput>
-) => {
-  await prisma.category.create({ data })
+export const createCategory = async (payload: Prisma.CategoryCreateArgs) => {
+  await prisma.category.create(payload)
   revalidatePath('/dashboard/categories')
 }
 
 export const updateCategory = async ({ where, data }: Prisma.CategoryUpdateArgs) => {
   await prisma.category.update({ where, data })
+  revalidatePath('/dashboard/categories')
+}
+
+export const deleteCategory = async (payload: Prisma.CategoryDeleteArgs) => {
+  await prisma.category.delete(payload)
   revalidatePath('/dashboard/categories')
 }
