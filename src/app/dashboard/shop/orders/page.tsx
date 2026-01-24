07291195464958b862ce8d457ee173971381b574
selfdestroying <max.f99@yandex.ru>
2026-01-24 20:21:33 +0300

    refactor: enhance order management with status change functionality and UI components

--- src/app/dashboard/shop/orders/page.tsx
+++ src/app/dashboard/shop/orders/page.tsx
@@ -1,8 +1,32 @@
 import { getOrders } from '@/actions/orders'
 import OrdersTable from '@/app/dashboard/shop/orders/_components/orders-table'
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
 
 export default async function Page() {
-  const orders = await getOrders()
+  const orders = await getOrders({
+    include: { product: true, student: true },
+    orderBy: { createdAt: 'desc' },
+  })
 
-  return <OrdersTable orders={orders} />
+  return (
+    <div className="grid min-h-0 flex-1 grid-cols-1">
+      <Card>
+        <CardHeader>
+          <CardTitle>Заказы</CardTitle>
+          <CardDescription>Список всех заказов системы</CardDescription>
+          <CardAction></CardAction>
+        </CardHeader>
+        <CardContent className="overflow-hidden">
+          <OrdersTable data={orders} />
+        </CardContent>
+      </Card>
+    </div>
+  )
 }
