63e503b35427503d96cf187ebf1e744ff6b62178
Maxim <max.f99@yandex.ru>
2026-03-07 14:05:28 +0300

    feat: create hint component

---
+++ src/components/hint.tsx
@@ -0,0 +1,47 @@
+import { CircleAlert, CircleCheck, CircleHelp, CircleX, Info, TriangleAlert } from "lucide-react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "./ui/tooltip";
+import { Button } from "./ui/button";
+import { cva } from "class-variance-authority";
+
+interface HintProps {
+    text: string
+    variant?: 'default' | 'destructive' | 'warning' | 'success'
+}
+
+const hintVariants = cva(
+    "text-sm px-3 py-1.5 rounded-md",
+    {
+        variants: {
+            variant: {
+                default: "",
+                destructive: "text-destructive hover:text-destructive hover:bg-destructive/20 dark:hover:bg-destructive/20",
+                warning: "text-warning hover:text-warning hover:bg-warning/20 dark:hover:bg-warning/20",
+                success: "text-success hover:text-success hover:bg-success/20 dark:hover:bg-success/20",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+        },
+    }
+)
+const iconMap = {
+    default: CircleHelp,
+    destructive: CircleX,
+    warning: CircleAlert,
+    success: CircleCheck,
+} as const
+
+export default function Hint({ text, variant = 'default' }: HintProps) {
+    const Icon = iconMap[variant]
+
+    return <Tooltip>
+        <TooltipTrigger render={
+            <Button size='icon-sm' variant='ghost' className={hintVariants({ variant })}>
+                <Icon />
+            </Button>
+        } />
+        <TooltipContent>
+            <p>{text}</p>
+        </TooltipContent>
+    </Tooltip>
+}
